5e1546fab8595bc2f242a480ab96eac2e82c207e
John-IvIv <econova2014@gmail.com>
2025-01-25 21:54:57 +0200

    study-2

--- js/study-2.js
+++ js/study-2.js
@@ -1,14 +1,6 @@
-// function checkAge(age) {
-//     if (checkAge) {
-//        checkAge = age >= 18;
-//          }
-//     }
-//     console.log(`You are an adult`)
-function makeTransaction(quantity, pricePerDroid) {
-    return `You ordered ${quantity} droids worth ${
-      pricePerDroid * quantity
-    } credits!`;
-  }
-  console.log(makeTransaction(5, 3000));
-  console.log(makeTransaction(3, 1000));
-  console.log(makeTransaction(10, 500));
+function checkAge(age) {
+    if (checkAge) {
+       checkAge = age >= 18;
+         }
+    }
+    console.log(`You are an adult`)
